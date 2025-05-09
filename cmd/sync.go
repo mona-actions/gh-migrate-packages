0b7bc48a4561c584b72e87c49db8d92e72161310
mark-humane <95398804+mark-humane@users.noreply.github.com>
2025-05-09 14:30:11 -0700

    testing out hostname

--- cmd/sync.go
+++ cmd/sync.go
@@ -15,7 +15,7 @@ var syncCmd = &cobra.Command{
 	Long:  "syncs packages to the target organization",
 	Run: func(cmd *cobra.Command, args []string) {
 		GetFlagOrEnv(cmd, map[string]bool{
-			"GHMPKG_TARGET_HOSTNAME":     false,
+			"GHMPKG_TARGET_HOSTNAME":     true,
 			"GHMPKG_TARGET_ORGANIZATION": true,
 			"GHMPKG_TARGET_TOKEN":        true,
 		})
@@ -29,12 +29,12 @@ var syncCmd = &cobra.Command{
 }
 
 func init() {
-	//syncCmd.Flags().StringP("target-hostname", "n", "", "GitHub Enterprise Server hostname URL (optional)")
+	syncCmd.Flags().StringP("target-hostname", "n", "", "GitHub Enterprise Server hostname URL (optional)")
 	syncCmd.Flags().StringP("source-organization", "o", "", "Organization (required)")
 	syncCmd.Flags().StringP("target-organization", "p", "", "Organization (required)")
 	syncCmd.Flags().StringP("target-token", "t", "", "GitHub token (required)")
 
-	//viper.BindPFlag("GHMPKG_TARGET_HOSTNAME", syncCmd.Flags().Lookup("target-hostname"))
+	viper.BindPFlag("GHMPKG_TARGET_HOSTNAME", syncCmd.Flags().Lookup("target-hostname"))
 	viper.BindPFlag("GHMPKG_SOURCE_ORGANIZATION", syncCmd.Flags().Lookup("source-organization"))
 	viper.BindPFlag("GHMPKG_TARGET_ORGANIZATION", syncCmd.Flags().Lookup("target-organization"))
 	viper.BindPFlag("GHMPKG_TARGET_TOKEN", syncCmd.Flags().Lookup("target-token"))
